52223f0f75577291784eeadb95b64facab830755
qhlai <qhlai666@gmail.com>
2023-06-18 23:22:54 +0800

    add keyframe func(unfinished)

--- colive_backend/src/communicator/client/communicator_client.hpp
+++ colive_backend/src/communicator/client/communicator_client.hpp
@@ -85,7 +85,37 @@ public:
     //     std::unique_lock<std::mutex>(mtx_kf_queue_);
     //     kf_out_buffer_.push_back(kf);
     // }
- 
+    virtual auto TryPassKeyPcToComm(PointCloudEX* pc)                                          ->void {
+
+        Vector3Type pos_diff = pc->pos_w-last_pos_;
+        // Eigen::Quaternion<double> quan_now=pc->quan_;
+        QuaternionType quan_diff =pc->quan_.conjugate()*last_quan_;
+        // quan_diff=quan_diff.normalize();
+// 四元数的减法运算(逆/共轭)得到的结果不一定是一个合法的四元数。
+// 因此，在进行减法运算时，需要对结果进行规范化操作，以确保其仍然是一个合法的四元数。
+
+            // odom_pose_prev = odom_pose_curr;
+            // odom_pose_curr = pose_curr;
+            // Pose6D dtf = diffTransformation(odom_pose_prev, odom_pose_curr); // dtf means delta_transform
+
+            // double delta_translation = sqrt(dtf.x*dtf.x + dtf.y*dtf.y + dtf.z*dtf.z); // note: absolute value. 
+            // translationAccumulated += delta_translation;
+            // rotaionAccumulated += (dtf.roll + dtf.pitch + dtf.yaw); // sum just naive approach.  
+
+            // // 关键帧选择
+            // if( translationAccumulated > keyframeMeterGap || rotaionAccumulated > keyframeRadGap ) {
+            //     isNowKeyFrame = true;
+            //     translationAccumulated = 0.0; // reset 
+            //     rotaionAccumulated = 0.0; // reset 
+            // } else {
+            //     isNowKeyFrame = false;
+            // }
+
+        PassPcToComm(pc);
+        last_pos_=pc->pos_w;
+        last_quan_=pc->quan_;
+
+    }
     virtual auto PassPcToComm(PointCloudEX* pc)                                             ->void {
         std::unique_lock<std::mutex>(mtx_pc_queue_);
         pointcloud_out_buffer_.push_back(pc);
